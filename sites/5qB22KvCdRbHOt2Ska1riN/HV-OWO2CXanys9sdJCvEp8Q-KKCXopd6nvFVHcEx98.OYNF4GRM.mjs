1c731fef556b2fc9e443fd12d7222d97e7c3cd1b
zkCryptoBiz <158302791+zkCryptoBiz@users.noreply.github.com>
2024-09-12 11:16:36 +0300

    Update HV-OWO2CXanys9sdJCvEp8Q-KKCXopd6nvFVHcEx98.OYNF4GRM.mjs

--- sites/5qB22KvCdRbHOt2Ska1riN/HV-OWO2CXanys9sdJCvEp8Q-KKCXopd6nvFVHcEx98.OYNF4GRM.mjs
+++ sites/5qB22KvCdRbHOt2Ska1riN/HV-OWO2CXanys9sdJCvEp8Q-KKCXopd6nvFVHcEx98.OYNF4GRM.mjs
@@ -3490,7 +3490,7 @@ var ri = { duration: 0, type: 'tween' },
                           BrUi5h7tU: 'rgb(255, 255, 255)',
                           bZsnTBIZu: 'rgb(21, 39, 102)',
                           CiWo9qyo0:
-                            'https://raydium.io/swap/?inputMint=sol&outputMint=PEPtXgB69okFK8SH7pfjpYDS9d86eNBWwmcbJituFfS',
+                            'https://pump.fun/Tdaviez9NXQM3cjQBwrJDAmzVtEwHMzAyAjNd8cpump',
                           height: '100%',
                           id: 'gPkRTpH7K',
                           layoutId: 'gPkRTpH7K',
@@ -5590,7 +5590,7 @@ ee(Vr, [{ explicitInter: !0, fonts: [] }, ...xl], {
 });
 g();
 var ja = (e) => (t) => {
-  let a = 'PEPtXgB69okFK8SH7pfjpYDS9d86eNBWwmcbJituFfS';
+  let a = 'Tdaviez9NXQM3cjQBwrJDAmzVtEwHMzAyAjNd8cpump';
   return r(e, {
     ...t,
     onClick: (o) => {
@@ -5927,7 +5927,7 @@ var ql = {
                                   BrUi5h7tU: 'rgb(255, 255, 255)',
                                   bZsnTBIZu: 'rgb(21, 39, 102)',
                                   CiWo9qyo0:
-                                    'https://raydium.io/swap/?inputMint=sol&outputMint=PEPtXgB69okFK8SH7pfjpYDS9d86eNBWwmcbJituFfS',
+                                    'https://pump.fun/Tdaviez9NXQM3cjQBwrJDAmzVtEwHMzAyAjNd8cpump',
                                   height: '100%',
                                   id: 'hhoducbSz',
                                   layoutId: 'hhoducbSz',
@@ -6121,7 +6121,7 @@ var ql = {
                                       BrUi5h7tU: 'rgb(255, 255, 255)',
                                       bZsnTBIZu: 'rgb(255, 124, 119)',
                                       CiWo9qyo0:
-                                        'https://raydium.io/swap/?inputMint=sol&outputMint=PEPtXgB69okFK8SH7pfjpYDS9d86eNBWwmcbJituFfS',
+                                        'https://pump.fun/Tdaviez9NXQM3cjQBwrJDAmzVtEwHMzAyAjNd8cpump',
                                       height: '100%',
                                       id: 'pWHJ7jW57',
                                       layoutId: 'pWHJ7jW57',
@@ -6139,7 +6139,7 @@ var ql = {
                                     children: r(er, {
                                       bZsnTBIZu: 'rgb(21, 39, 102)',
                                       CiWo9qyo0:
-                                        'https://www.dextools.io/app/en/solana/pair-explorer/PEPtXgB69okFK8SH7pfjpYDS9d86eNBWwmcbJituFfS',
+                                        'https://www.dextools.io/app/en/solana/pair-explorer/Tdaviez9NXQM3cjQBwrJDAmzVtEwHMzAyAjNd8cpump',
                                       height: '100%',
                                       id: 'cnbvAVx7A',
                                       jkUJRz9gR: Qr(
@@ -6413,7 +6413,7 @@ var ql = {
                                                   'rgb(59, 91, 255)',
                                               },
                                               children:
-                                                'PEPtXgB69okFK8SH7pfjpYDS9d86eNBWwmcbJituFfS',
+                                                'Tdaviez9NXQM3cjQBwrJDAmzVtEwHMzAyAjNd8cpump',
                                             }),
                                           }),
                                         },
@@ -6434,7 +6434,7 @@ var ql = {
                                                 'rgb(59, 91, 255)',
                                             },
                                             children:
-                                              'PEPtXgB69okFK8SH7pfjpYDS9d86eNBWwmcbJituFfS',
+                                              'Tdaviez9NXQM3cjQBwrJDAmzVtEwHMzAyAjNd8cpump',
                                           }),
                                         }),
                                         className: 'framer-1t261v',
@@ -7663,7 +7663,7 @@ var ql = {
                                             '--framer-text-color':
                                               'rgb(255, 255, 255)',
                                           },
-                                          children: '100.000.000',
+                                          children: '1.000.000.000',
                                         }),
                                         r('p', {
                                           style: {
@@ -7695,7 +7695,7 @@ var ql = {
                                             '--framer-text-color':
                                               'rgb(255, 255, 255)',
                                           },
-                                          children: '100.000.000',
+                                          children: '1.000.000.000',
                                         }),
                                         r('p', {
                                           style: {
@@ -7729,7 +7729,7 @@ var ql = {
                                           '--framer-text-color':
                                             'rgb(255, 255, 255)',
                                         },
-                                        children: '100.000.000',
+                                        children: '1.000.000.000',
                                       }),
                                       r('p', {
                                         style: {
@@ -8075,7 +8075,7 @@ var ql = {
                                       children: r(er, {
                                         bZsnTBIZu: 'rgb(21, 39, 102)',
                                         CiWo9qyo0:
-                                          'https://dexscreener.com/solana/PEPtXgB69okFK8SH7pfjpYDS9d86eNBWwmcbJituFfS',
+                                          'https://dexscreener.com/solana/Tdaviez9NXQM3cjQBwrJDAmzVtEwHMzAyAjNd8cpump',
                                         height: '100%',
                                         id: 'bOHANc5rB',
                                         jkUJRz9gR: Qr(
@@ -8097,7 +8097,7 @@ var ql = {
                                       children: r(er, {
                                         bZsnTBIZu: 'rgb(21, 39, 102)',
                                         CiWo9qyo0:
-                                          'https://www.dextools.io/app/en/solana/pair-explorer/PEPtXgB69okFK8SH7pfjpYDS9d86eNBWwmcbJituFfS',
+                                          'https://www.dextools.io/app/en/solana/pair-explorer/Tdaviez9NXQM3cjQBwrJDAmzVtEwHMzAyAjNd8cpump',
                                         height: '100%',
                                         id: 'sHW9lmejQ',
                                         jkUJRz9gR: Qr(
